6e66f46da0bb285be59c0e41a66a8e9f5cf75fb6
Anthonyysilv <anthony.silva@aluno.ifsp.edu.br>
2024-03-26 12:20:57 -0300

    26/3 12:20

--- Lista 4 - Beecrowd Manipulação de Strings/1222.php
+++ Lista 4 - Beecrowd Manipulação de Strings/1222.php
@@ -1,23 +1,23 @@
 <?php
-while ($entrada = fgets(STDIN)) {
-    list($numeroPalavrasConto, $numeroLinha, $caracteresLinha) = explode(" ", $entrada);
-    $paginas = $linhas = 1;
-    $conto = readline();
-    $numeroPalavrasConto--;
-    $conto = explode(" ", $conto);
-    $letras = 0;
-    for ($i = 0; $i < $numeroPalavrasConto; $i++) {
-        $x = strlen($conto[$i]);
-        if (($letras + $x + 1) <= $caracteresLinha)
-            $letras += $x + 1;
-        else {
-            ++$linhas;
-            if ($linhas > $numeroLinha) {
-                ++$paginas;
-                $linhas = 1;
-                $letras = 0;
-            }
+#incompleto
+    $in = array_map('intval', explode(" ", readline()));
+    $n = $in[0];
+    $l = $in[1];
+    $c = $in[2];
+    /*$n = intval($n);
+    $l = intval($l);
+    $c = intval($c);*/
+    $palavras = explode(" ", readline());
+    $tamLinha = 0;
+    for($i=0; $i<$n; $i++)
+    {
+        $tamLinha += strlen($palavras[$i]) + 1;
+        if($tamLinha > $c)
+        {
+            $linhas++;
+            $tamLinha += strlen($palavras[$i]) + 1;
         }
     }
-    echo "$paginas\n";
-}
+    $pag = ceil($linhas/$l);
+    echo $pag."\n";
+?>
