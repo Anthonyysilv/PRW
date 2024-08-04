8b8b37effca8de17aaf61bdc6fdb8893b7f7c067
Anthonyysilv <anthony.silva@aluno.ifsp.edu.br>
2024-08-04 10:13:04 -0300

    Cadastro e banco

---
+++ Prova2/includes/conexao.php
@@ -0,0 +1,16 @@
+<?php
+    //Informações do banco
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'Prova2';
+    $port     = 3307; //Alterar para 3307 no IF
+    //Conecta com o banco
+    $con = mysqli_connect($hostname, $username, $password, $database, $port);
+    //Verifica a conexão
+    if (mysqli_connect_error())
+    {
+        printf("Erro de conexão: %s", mysqli_connect_error());
+        exit();
+    }
+?>
