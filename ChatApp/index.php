3c2a85d207e832847d98551c7131b338534e1e99
Anthonyysilv <anthony.silva@aluno.ifsp.edu.br>
2024-08-10 22:51:17 -0300

    ChatApp

--- ChatApp/index.php
+++ ChatApp/index.php
@@ -1,43 +1,48 @@
-<?php include_once "header.php";?>
+<?php
+    session_start();
+    if(isset($_SESSION['unique_id'])) {
+        header("location: users.php");
+    }
+?>
+<?php include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="form signup">
             <header>Realtime Chat App</header>
             <form action="#" method="post" enctype="multipart/form-data" autocomplete="off">
                 <div class="error-text"></div>
+                <div class="field input"></div>
                 <div class="name-details">
                     <div class="field input">
-                        <label>First Name</label>
+                        <label for="">First Name</label>
                         <input type="text" name="fname" placeholder="First name" required>
                     </div>
                     <div class="field input">
-                        <label>Last Name</label>
+                        <label for="">Last Name</label>
                         <input type="text" name="lname" placeholder="Last name" required>
                     </div>
                 </div>
-                <div class="field input">
+                <div class="fiedl input">
                     <label for="">Email Address</label>
-                    <input type="text" name="email" placeholder="Enter Your Email" required>
+                    <input type="text" name="email" placeholder="Enter your email" required>
                 </div>
                 <div class="field input">
                     <label for="">Password</label>
-                    <input type="password" name="password" placeholder="Enter New Password" required>
+                    <input type="password" name="password" placeholder="Enter new password" required>
                     <i class="fas fa-eye"></i>
                 </div>
                 <div class="field image">
-                    <!--IMPORTANTE-->
-                    <label for="">Profile Image</label>
-                    <!--Adiciona imagem do computador-->
-                    <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jgp" required> 
+                    <label for="">Select Image</label>
+                    <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
                 </div>
                 <div class="field button">
                     <input type="submit" name="submit" value="Continue to Chat">
                 </div>
             </form>
-            <div class="link">Already signed up? <a href="login.php"> Login now</a></div>
+            <div class="link">Already signed up? <a href="login.php">Login now</a></div>
         </section>
     </div>
-    <script type="text/javascript" src="js/pass-show-hide.js"></script>
-    <script type="tetx/javascript" src="js/signup.js"></script>
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/signup.js"></script>
 </body>
 </html>
