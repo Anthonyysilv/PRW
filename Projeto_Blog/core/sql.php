b5e9ea6b0a6dd82367ff8f256a80b2428a10bdb3
Anthonyysilv <anthony.silva@aluno.ifsp.edu.br>
2024-09-19 10:09:36 -0300

    instruções sql

---
+++ Projeto_Blog/core/sql.php
@@ -0,0 +1,63 @@
+<?php
+    #arquivo que gera as instruções automaticamente
+    function insert(string $entidade, array $dados) : string
+    {
+        $instrucao = "INSERT INTO {$entidade}";
+
+        $campos = implode(', ', array_keys($dados));
+        $valores = implode(', ', array_values($dados));
+
+        $instrucao .= "({$campos})";
+        $instrucao .= "VALUES ({$valores})";
+        return $instrucao;
+    }
+    function update(string $entidade, array $dados, array $criterio = []) : string
+    {
+        $instrucao = "UPDATE {$entidade}";
+
+        foreach ($dados as $campo => $dado) {
+            $set[] = "{$campo} = {$dado}";
+        }
+
+        $instrucao .= 'SET '.implode(', ', $set);
+
+        if(!empty($criterio)) {
+            $instrucao .= ' WHERE ';
+
+            foreach($criterio as $expressao) {
+                $instrucao .= ' '.implode(' ', $expressao);
+            }
+        }
+        return $instrucao;
+    }
+    function delete(string $entidade, array $criterio = []) : string
+    {
+        $instrucao = "DELETE {$entidade}";
+
+        if(!empty($criterio)) {
+            $instrucao .= ' WHERE ';
+
+            foreach($criterio as $expressao) {
+                $instrucao .= ' '.implode(' ', $expressao);
+            }
+        }
+        return $instrucao;
+    }
+    function select(string $entidade, array $campos, array $criterio = [], string $ordem = null) : string
+    {
+        $instrucao = "SELECT ". implode(', ', $campos);
+        $instrucao .= " From {$entidade}";
+
+        if(!empty($criterio)) {
+            $instrucao .= ' WHERE ';
+
+            foreach($criterio as $expressao) {
+                $instrucao .= ' ' . implode(' ', $expressao);
+            }
+        }
+        if(!empty($ordem)) {
+            $instrucao .= "ORDER BY $ordem";
+        }
+        return $instrucao;
+    }
+?>
