521951f9d441877e44291322cd55894555708bf3
Anthonyysilv <anthony.silva@aluno.ifsp.edu.br>
2024-10-10 09:08:25 -0300

    Projeto Blog foi enviado para um rep. próprio

--- Projeto_Blog/includes/busca.php
+++
@@ -1,4 +0,0 @@
-<form class="form-inline my-2 my-lg-0" method="get" action="">
-    <input type="search" class="form-control mr-sm-2" name="busca" placeholder="Busca" aria-label="Busca">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-</form>
